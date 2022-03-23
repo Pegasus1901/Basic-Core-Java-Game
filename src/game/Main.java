e3ec8d386f3e2379eefd9ef46901a1ecec74e226
Prathmesh Sanjay Shevale <shevaleprathmesh8@gmail.com>
2022-03-23 19:02:39 +0530

    removed Comments

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,12 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Player1 player = new Player1("Prathmesh","Ak-47",200);
 		
-//		System.out.println(player.getName());
-//		System.out.println(player.getWeapon());
-//		System.out.println(player.getHealth());
-//		player.damageByGun1();
-//		player.damageByGun1();
-//		player.damageByGun2();
+		player.damageByGun1();
+		player.damageByGun1();
+		player.heal();
 		
 		Player2 betterPlayer= new Player2("Google","Machine Gun",100,true);
 		}
